5d2b27f0107c7098cea2fa53db90b55c2b8feb01
hajunsteppa-ui <hajunsteppa@gmail.com>
2026-06-01 16:30:26 +0800

    Update war.js

--- Rudra/commands/war.js
+++ Rudra/commands/war.js
@@ -22,30 +22,32 @@ module.exports.run = async function({ api, args, Users, event}) {
     var a = function (a) { api.sendMessage(a, event.threadID); }
 a("Listen to your father, kids !");
 setTimeout(() => {a({body: "F*ck your mother" })}, 3000);
-setTimeout(() => {a({body: "You little brats come out to listen to your father curse"})}, 5000);
-setTimeout(() => {a({body: "Quick show the dogs" })}, 7000);
-setTimeout(() => {a({body: "Show your father's soul" })}, 9000);
-setTimeout(() => {a({body: "Do you guys like war so much?" })}, 12000);
-setTimeout(() => {a({body: "Damn you guys too" })}, 15000);
-setTimeout(() => {a({body: "Give your father the age of war" })}, 17000);
-setTimeout(() => {a({body: "Hurry up and curse each other with me" })}, 20000);
-setTimeout(() => {a({body: "Are the bad boys wrinkling their noses up to wage war on your father?" })}, 23000);
-setTimeout(() => {a({body: "I fuck your mother" })}, 25000);
-setTimeout(() => {a({body: "Delicious then yawn your mother up" })}, 28500);
-setTimeout(() => {a({body: "Your father shot you to death by rapping" })}, 31000);
-setTimeout(() => {a({body: "Please age eat me ?" })}, 36000);
-setTimeout(() => {a({body: "If it's delicious, eat your dad" })}, 39000);
-setTimeout(() => {a({body: "Before that, please give me a break for 1 minute" })}, 40000);
-setTimeout(() => {a({body: "Please allow me to start" })}, 65000);
-setTimeout(() => {a({body: "First of all, I would like to fuck you from top to bottom" })}, 70000);
-setTimeout(() => {a({body: "I fuck from cunt hole to pussy cleavage" })}, 75000);
-setTimeout(() => {a({body: "The cunt is as big as a buffalo's cunt masturbating a sewer pipe" })}, 80000);
-setTimeout(() => {a({body: "I'm sure 2 guys like me aren't enough to fill your ass hole" })}, 85000);
-setTimeout(() => {a("I'm tired and don't curse anymore")} , 90000);
-setTimeout(() => {a({body: "Come on boss update the lyric, let's continue the war" })}, 95000);
-setTimeout(() => {a({body: "Thank you for listening to me war" })}, 100000);
-setTimeout(() => {a({body: "Goodbye and see you in the next program" })}, 105000);
-setTimeout(() => {a({body: "Good bye 🥺"})} , 115000);
+setTimeout(() => {a({body: "Hello everyone!" })}, 5000);
+setTimeout(() => {a({body: "You creative friends come out to listen to your leader chat"})}, 10000);
+setTimeout(() => {a({body: "Quick, show your best skills" })}, 15000);
+setTimeout(() => {a({body: "Show your true spirit" })}, 20000);
+setTimeout(() => {a({body: "Do you guys like friendly competition so much?" })}, 25000);
+setTimeout(() => {a({body: "Awesome work, you guys too" })}, 30000);
+setTimeout(() => {a({body: "Give your leader a great match" })}, 35000);
+setTimeout(() => {a({body: "Hurry up and cheer each other on with me" })}, 40000);
+setTimeout(() => {a({body: "Are the eager players getting ready to challenge the leader?" })}, 45000);
+setTimeout(() => {a({body: "I respect your effort" })}, 50000);
+setTimeout(() => {a({body: "If it's wonderful, then lift your team up" })}, 55000);
+setTimeout(() => {a({body: "Your leader amazed you by rapping" })}, 60000);
+setTimeout(() => {a({body: "Please enjoy the show" })}, 65000);
+setTimeout(() => {a({body: "If it's amazing, support your leader" })}, 70000);
+setTimeout(() => {a({body: "Before that, please give me a break for 1 minute" })}, 75000);
+setTimeout(() => {a({body: "Please allow me to start" })}, 135000);
+setTimeout(() => {a({body: "First of all, I would like to welcome you from top to bottom" })}, 140000);
+setTimeout(() => {a({body: "I rhyme from the beat to the melody" })}, 145000);
+setTimeout(() => {a({body: "The energy is as big as a stadium filling up the whole city" })}, 150000);
+setTimeout(() => {a({body: "I'm sure just a few lines like mine aren't enough to show all the hype" })}, 155000);
+setTimeout(() => {a("I'm catching my breath and taking a quick break")} , 160000);
+setTimeout(() => {a({body: "Come on team, update the lyrics, let's continue the match" })}, 165000);
+setTimeout(() => {a({body: "Thank you for listening to my performance" })}, 170000);
+setTimeout(() => {a({body: "Goodbye and see you in the next program" })}, 175000);
+setTimeout(() => {a({body: "Good bye 🥺"})} , 180000);
+    
 
 
 
